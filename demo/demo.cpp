b81d13b3d091207dfe13e90981066ef3b7fdbb5f
ancarola <raffaele.ancarola@epfl.ch>
2019-09-21 00:52:04 +0200

    Finally it compiles

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,20 +1,17 @@
-#include <flatland/flatland.hpp>
+#include <wsdl2/event.hpp>
 
+#include <flatland/flatland.hpp>
 #include <flatland/core/task.hpp>
 #include <flatland/core/signal.hpp>
 
-#include <flatland/window.hpp>
 #include <flatland/sprite.hpp>
 #include <flatland/debug.hpp>
 
-#include <wsdl2/event.hpp>
-
-#include <sol/sol.hpp>
-
+#include <flatlua/lua_state.hpp>
 
 void keypressed(const wsdl2::event::key event) {
     if (event.type == wsdl2::event::key::action::down) {
-        if (event.keysym.sym == SDLK_ESCAPE) {
+        if (event.code() == SDLK_e) {
             flat::state::get().running = false;
         }
     }
@@ -35,15 +32,23 @@ int main() {
         }
     );
 
-    win.insert(std::make_shared<flat::theater>());
+    //win.insert(std::make_shared<flat::theater>());
     win.open();
 
     // Open lua state
+    flat::lua::state lua_state(engine);
+
+    auto demo_result = lua_state.load("scripts/demo.lua");
+
+    if (!demo_result.valid())
+    {
+        npdebug("Could not load demo.lua correctly")
+    }
 
     // Run flatland engine loop
     flat::run();
 
-    // Quit flatland engine
+    // TODO, avoid this call, Quit flatland engine
     flat::quit();
 
     return 0;
--- demo/scripts/demo.lua
+++ demo/scripts/demo.lua
@@ -3,25 +3,35 @@ local angle = 0
 local radius = 0.5
 
 -- connect: from flatland
-connect(events.key, 
-    function (event)
-        if event.type_ == events.key.action.down and 
-           event.key == events.key.ESC then
-           quit() -- from flatland
-        end
-    end) -- ESC
+local fb = connect("quit_key", events.key, 
+        function (event)
+            if event.action_ == keys.action.down and 
+                event.code() == keys.escape then
+                quit() -- from flatland
+            end
+        end) -- ESC
 
-connect(events.key,
-    function (event)
-        if event.type_ == events.key.action.down and 
-           event.key == events.key.A then
-            local x = radius * math.cos(angle);
-            local y = radius * math.sin(angle);
-            print("(" .. x .. ", " .. y .. ")")
-        end
-    end) -- A
+if not fb then
+    error("quit_key not bound correctly")
+end
+
+fb = connect("mod_key", events.key,
+        function (event)
+            if event.action_ == keys.action.down and 
+               event.code() == keys.A then
+                local x = radius * math.cos(angle);
+                local y = radius * math.sin(angle);
+                print("(" .. x .. ", " .. y .. ")")
+            end
+        end) -- A
+
+if not fb then
+    error("mod_key not bound correctly")
+end
 
 -- update: from flatland
-update(function ()
-    angle += 0.001
-end
+flat.update.delegate_task(
+    function ()
+        angle += 0.001
+    end, 
+    1) -- priority 1
